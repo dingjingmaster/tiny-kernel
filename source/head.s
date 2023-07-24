620c37ed92086501c0c5222109693c7b82e9d408
dingjingmaster <dingjing@live.cn>
2023-07-24 16:07:40 +0800

    暂存

--- source/head.s
+++ source/head.s
@@ -9,16 +9,16 @@ LDT1_SEL	= 0x38
 .global startup_32
 .text
 startup_32:
-	movl $0x10,%eax
+	movl $0x10,%eax             # 
 	mov %ax,%ds
 #	mov %ax,%es
-	lss init_stack,%esp
+	lss init_stack,%esp         # 将 init_stack 的地址赋值给 %esp 寄存器
 
 # setup base fields of descriptors.
 	call setup_idt
 	call setup_gdt
-	movl $0x10,%eax		# reload all the segment registers
-	mov %ax,%ds		# after changing gdt. 
+	movl $0x10,%eax		        # reload all the segment registers
+	mov %ax,%ds                 # after changing gdt. 
 	mov %ax,%es
 	mov %ax,%fs
 	mov %ax,%gs
@@ -28,7 +28,7 @@ startup_32:
 	movb $0x36, %al
 	movl $0x43, %edx
 	outb %al, %dx
-	movl $11930, %eax        # timer frequency 100 HZ 
+	movl $11930, %eax           # timer frequency 100 HZ 
 	movl $0x40, %edx
 	outb %al, %dx
 	movb %ah, %al
@@ -38,7 +38,7 @@ startup_32:
 	movl $0x00080000, %eax	
 	movw $timer_interrupt, %ax
 	movw $0x8E00, %dx
-	movl $0x08, %ecx              # The PC default timer int.
+	movl $0x08, %ecx            # The PC default timer int.
 	lea idt(,%ecx,8), %esi
 	movl %eax,(%esi) 
 	movl %edx,4(%esi)
@@ -173,19 +173,19 @@ scr_loc:.long 0
 
 .align 2
 lidt_opcode:
-	.word 256*8-1		# idt contains 256 entries
-	.long idt		# This will be rewrite by code. 
+	.word 256*8-1           # idt contains 256 entries
+	.long idt               # This will be rewrite by code. 
 lgdt_opcode:
-	.word (end_gdt-gdt)-1	# so does gdt 
-	.long gdt		# This will be rewrite by code.
+	.word (end_gdt-gdt)-1   # so does gdt 
+	.long gdt               # This will be rewrite by code.
 
 	.align 8
-idt:	.fill 256,8,0		# idt is uninitialized
+idt:	.fill 256,8,0       # idt is uninitialized
 
-gdt:	.quad 0x0000000000000000	/* NULL descriptor */
-	.quad 0x00c09a00000007ff	/* 8Mb 0x08, base = 0x00000 */
-	.quad 0x00c09200000007ff	/* 8Mb 0x10 */
-	.quad 0x00c0920b80000002	/* screen 0x18 - for display */
+gdt:	.quad 0x0000000000000000    /* NULL descriptor */
+	.quad 0x00c09a00000007ff        /* 8Mb 0x08, base = 0x00000 */
+	.quad 0x00c09200000007ff        /* 8Mb 0x10 */
+	.quad 0x00c0920b80000002        /* screen 0x18 - for display */
 
 	.word 0x0068, tss0, 0xe900, 0x0	# TSS0 descr 0x20
 	.word 0x0040, ldt0, 0xe200, 0x0	# LDT0 descr 0x28
@@ -193,23 +193,23 @@ gdt:	.quad 0x0000000000000000	/* NULL descriptor */
 	.word 0x0040, ldt1, 0xe200, 0x0	# LDT1 descr 0x38
 end_gdt:
 	.fill 128,4,0
-init_stack:                          # Will be used as user stack for task0.
+init_stack:                         # Will be used as user stack for task0.
 	.long init_stack
 	.word 0x10
 
 /*************************************/
 .align 8
 ldt0:	.quad 0x0000000000000000
-	.quad 0x00c0fa00000003ff	# 0x0f, base = 0x00000
-	.quad 0x00c0f200000003ff	# 0x17
+	.quad 0x00c0fa00000003ff        # 0x0f, base = 0x00000
+	.quad 0x00c0f200000003ff        # 0x17
 
-tss0:	.long 0 			/* back link */
-	.long krn_stk0, 0x10		/* esp0, ss0 */
-	.long 0, 0, 0, 0, 0		/* esp1, ss1, esp2, ss2, cr3 */
-	.long 0, 0, 0, 0, 0		/* eip, eflags, eax, ecx, edx */
-	.long 0, 0, 0, 0, 0		/* ebx esp, ebp, esi, edi */
-	.long 0, 0, 0, 0, 0, 0 		/* es, cs, ss, ds, fs, gs */
-	.long LDT0_SEL, 0x8000000	/* ldt, trace bitmap */
+tss0:	.long 0                     /* back link */
+	.long krn_stk0, 0x10            /* esp0, ss0 */
+	.long 0, 0, 0, 0, 0             /* esp1, ss1, esp2, ss2, cr3 */
+	.long 0, 0, 0, 0, 0             /* eip, eflags, eax, ecx, edx */
+	.long 0, 0, 0, 0, 0             /* ebx esp, ebp, esi, edi */
+	.long 0, 0, 0, 0, 0, 0          /* es, cs, ss, ds, fs, gs */
+	.long LDT0_SEL, 0x8000000       /* ldt, trace bitmap */
 
 	.fill 128,4,0
 krn_stk0:
@@ -218,17 +218,17 @@ krn_stk0:
 /************************************/
 .align 8
 ldt1:	.quad 0x0000000000000000
-	.quad 0x00c0fa00000003ff	# 0x0f, base = 0x00000
-	.quad 0x00c0f200000003ff	# 0x17
+	.quad 0x00c0fa00000003ff        # 0x0f, base = 0x00000
+	.quad 0x00c0f200000003ff        # 0x17
 
-tss1:	.long 0 			/* back link */
-	.long krn_stk1, 0x10		/* esp0, ss0 */
-	.long 0, 0, 0, 0, 0		/* esp1, ss1, esp2, ss2, cr3 */
-	.long task1, 0x200		/* eip, eflags */
-	.long 0, 0, 0, 0		/* eax, ecx, edx, ebx */
-	.long usr_stk1, 0, 0, 0		/* esp, ebp, esi, edi */
+tss1:	.long 0                     /* back link */
+	.long krn_stk1, 0x10            /* esp0, ss0 */
+	.long 0, 0, 0, 0, 0             /* esp1, ss1, esp2, ss2, cr3 */
+	.long task1, 0x200              /* eip, eflags */
+	.long 0, 0, 0, 0                /* eax, ecx, edx, ebx */
+	.long usr_stk1, 0, 0, 0         /* esp, ebp, esi, edi */
 	.long 0x17,0x0f,0x17,0x17,0x17,0x17 /* es, cs, ss, ds, fs, gs */
-	.long LDT1_SEL, 0x8000000	/* ldt, trace bitmap */
+	.long LDT1_SEL, 0x8000000       /* ldt, trace bitmap */
 
 	.fill 128,4,0
 krn_stk1:
@@ -237,7 +237,7 @@ krn_stk1:
 task0:
 	movl $0x17, %eax
 	movw %ax, %ds
-	movb $65, %al              /* print 'A' */
+	movb $65, %al                   /* print 'A' */
 	int $0x80
 	movl $0xfff, %ecx
 1:	loop 1b
@@ -246,7 +246,7 @@ task0:
 task1:
 	movl $0x17, %eax
 	movw %ax, %ds
-	movb $66, %al              /* print 'B' */
+	movb $66, %al                   /* print 'B' */
 	int $0x80
 	movl $0xfff, %ecx
 1:	loop 1b
